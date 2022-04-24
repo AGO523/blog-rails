802521f25a9864a37d3e81b0a0aed07c45eac30a
AGO523 <unpouletfrais0523@gmail.com>
2022-04-24 14:11:49 +0900

    add: posts to validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   has_one_attached :top_image
 
   validates :title, length: { maximum: 50 }, presence: true
+  validates :image, content_type: [:png, :jpg, :jpeg], size: { less_than_or_equal_to: 10.megabytes }, dimension: { width: { max: 2000 }, height: { max: 2000 }}
+  
 end

d152a2088f5531b8a0bae35c9e148e039bbb14ff
AGO523 <unpouletfrais0523@gmail.com>
2022-04-24 14:23:08 +0900

    add: paginate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.with_rich_text_cotent
+    @posts = Post.with_rich_text_cotent.page(params[:page]).per(8)
   end
 
   # GET /posts/1 or /posts/1.json

252b57a22db227ea63c650de35869b04ac12c6e5
AGO523 <unpouletfrais0523@gmail.com>
2022-05-01 10:05:21 +0900

    add: comment layout

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
+
+  validates :name, length: { maximum: 30 }, presence: true
+  validates :content, length: { maximum: 500 }, presence: true
 end

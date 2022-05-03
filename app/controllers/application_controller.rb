98428dd909695016ff6f89c9ad2537c7d47100e3
AGO523 <unpouletfrais0523@gmail.com>
2022-05-04 07:06:04 +0900

    fix: 設定ファイルをコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  # rescue_from Exception, with: :error500
+  # rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
+  # def error404(e)
+  #   render "error404", status: 404, formats: [:html]
+  # end
+
+  # def error500(e)
+  #   logger.error [e, *e.backtrace].join("\n")
+  #   render "error500", status: 500, formats: [:html]
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   
+  root 'posts#index'
+
   resources :posts do
     resources :comments, only: %i[create]
   end
-  root 'posts#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  match "*path" => "application#error404", via: :all
 end
